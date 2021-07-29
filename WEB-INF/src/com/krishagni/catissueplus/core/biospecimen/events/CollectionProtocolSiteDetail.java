5daa619d42a627eb8c27f0817c73925fa05bd105
Vinayak Pawar <vinayakapawar@gmail.com>
2021-07-29 14:31:06 +0530

    OPSMN-5632: Coordinator workflow feedback incorporation/improvements
    1. Displayed collected/missed/not collected specimens in the read only
    mode in the tree widget. Only the pending specimens can be edited
    or collected.
    
    2. Added a cancel handler to navigate to right screen. The cancel
    on the tree widget was looping...
    
    3. Added ability to show or hide fields on the nth step based
    on the user role on the CP in action.
    
    4. Added ability to show or hide fields in the specimen collection
    tree widget bacsed on the role played by user on the CP.

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/events/CollectionProtocolSiteDetail.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/events/CollectionProtocolSiteDetail.java
@@ -3,14 +3,18 @@ package com.krishagni.catissueplus.core.biospecimen.events;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.krishagni.catissueplus.core.biospecimen.domain.CollectionProtocolSite;
+import com.krishagni.catissueplus.core.common.util.Utility;
 
 @JsonFilter("withoutId")
 public class CollectionProtocolSiteDetail {
 	private Long id;
-	
+
+	private Long siteId;
+
 	private String siteName;
 	
 	private String code;
@@ -21,6 +25,14 @@ public class CollectionProtocolSiteDetail {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Long getSiteId() {
+		return siteId;
+	}
+
+	public void setSiteId(Long siteId) {
+		this.siteId = siteId;
 	}
 
 	public String getSiteName() {
@@ -42,19 +54,13 @@ public class CollectionProtocolSiteDetail {
 	public static CollectionProtocolSiteDetail from(CollectionProtocolSite cpSite) {
 		CollectionProtocolSiteDetail detail = new CollectionProtocolSiteDetail();
 		detail.setId(cpSite.getId());
+		detail.setSiteId(cpSite.getSite().getId());
 		detail.setSiteName(cpSite.getSite().getName());
 		detail.setCode(cpSite.getCode());
-		
 		return detail;
 	}
 	
 	public static List<CollectionProtocolSiteDetail> from(Collection<CollectionProtocolSite> cpSites) {
-		List<CollectionProtocolSiteDetail> result = new ArrayList<CollectionProtocolSiteDetail>();
-		for (CollectionProtocolSite cpSite: cpSites) {
-			result.add(from(cpSite));
-		}
-		
-		return result;
+		return Utility.nullSafeStream(cpSites).map(CollectionProtocolSiteDetail::from).collect(Collectors.toList());
 	}
-	
 }
