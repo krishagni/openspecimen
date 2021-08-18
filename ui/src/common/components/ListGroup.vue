79caea14585d0aacb7c9dfca002f228eaba2410f
Vinayak Pawar <vinayakapawar@gmail.com>
2021-08-18 19:14:24 +0530

    List group component

---
+++ ui/src/common/components/ListGroup.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <Panel class="os-list-group-wrapper">
+    <template #header>
+      <slot name="header"></slot>
+    </template>
+
+    <div class="os-list-group">
+      <a v-for="(item, idx) of list" :key="idx"
+        class="item" :class="{active: item == selected}"
+        @click="$emit('on-item-select', {item: item, index: idx})">
+        <slot :item="item" :index="idx"></slot>
+      </a>
+    </div>
+  </Panel>
+</template>
+
+<script>
+
+import Panel from '@/common/components/Panel.vue';
+
+export default {
+  props: ['list', 'selected'],
+
+  components: {
+    Panel
+  }
+}
+
+</script>
+
+<style scoped>
+
+.os-list-group-wrapper :deep(.p-panel-content) {
+  padding: 0rem;
+}
+
+.os-list-group {
+  list-style: none;
+  padding: 0rem;
+  margin: 0rem;
+}
+
+.os-list-group .item {
+  padding: 1rem;
+  display: block;
+}
+
+.os-list-group .item:not(:first-child) {
+  border-top: 1px solid #ddd;
+}
+
+.os-list-group .item:hover {
+  background: #f7f7f7;
+}
+
+.os-list-group .item.active {
+  background-color: #007bff;
+  border-color: #007bff;
+  color: #fff;
+  z-index: 2;
+}
+
+.os-list-group a.item {
+  text-decoration: none;
+  cursor: pointer;
+  color: inherit;
+}
+
+</style>
