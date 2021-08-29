c5d1d011e7f45330b253ed2b6bbf9faf7048563a
Vinayak Pawar <vinayakapawar@gmail.com>
2021-08-29 09:19:01 +0530

    The topmost view - the root view. This view will load and cache all the required UI resources.

---
+++ ui/src/Root.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+
+export default {
+  name: 'Root',
+
+  components: {
+  },
+
+  setup() {
+  },
+
+  mounted() {
+  }
+}
+
+</script>
+
+<style>
+</style>
