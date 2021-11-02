335065a0d497ad86dc4b30b090a8fc46078a26fd
Vinayak Pawar <vinayakapawar@gmail.com>
2021-11-02 10:14:36 +0530

    Vue JS Upgrade: Navbar. Help button and its associated overlay options.

--- ui/src/common/components/Navbar.vue
+++ ui/src/common/components/Navbar.vue
@@ -17,6 +17,101 @@
         <span> </span>
       </div>
       <div class="buttons">
+        <div class="help">
+          <button @click="toggleHelpMenu">
+            <os-icon name="question-circle" />
+          </button>
+
+          <os-overlay ref="helpMenu" @click="toggleHelpMenu">
+            <ul class="help-options">
+              <li>
+                <a href="https://help.openspecimen.org" target="_blank" rel="noopener">
+                  <span>Online Help</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://forums.openspecimen.org" target="_blank" rel="noopener">
+                  <span>Q & A Forums</span>
+                </a>
+              </li>
+              <li class="divider">
+                <os-divider />
+              </li>
+              <li>
+                <a @click="showOpenSpecimenInfo()">
+                  <span>About OpenSpecimen</span>
+                </a>
+              </li>
+            </ul>
+          </os-overlay>
+
+          <os-dialog ref="aboutDialog">
+            <template #header>
+              <span>About OpenSpecimen</span>
+            </template>
+            <template #content>
+              <os-tabs>
+                <os-tab>
+                  <template #header>
+                    <os-icon-title icon="eye" title="Overview" />
+                  </template>
+
+                  <ul class="os-key-values">
+                    <li class="item">
+                      <strong class="key key-sm">Version</strong>
+                      <span class="value">{{$ui.global.appProps.build_version}}</span>
+                    </li>
+                    <li class="item">
+                      <strong class="key key-sm">Build Date</strong>
+                      <span class="value">{{$filters.date(+$ui.global.appProps.build_date)}}</span>
+                    </li>
+                    <li class="item">
+                      <strong class="key key-sm">Revision</strong>
+                      <span class="value">{{$ui.global.appProps.build_commit_revision}}</span>
+                    </li>
+                  </ul>
+                </os-tab>
+
+                <os-tab>
+                  <template #header>
+                    <os-icon-title icon="list" title="Plugins" />
+                  </template>
+
+                  <table class="os-table">
+                    <thead>
+                      <tr>
+                        <th>Plugin</th>
+                        <th>Version</th>
+                        <th>Build Date</th>
+                        <th>Revision</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="plugin in $ui.global.appProps.pluginsDetail" :key="plugin['os-plugin-name']">
+                        <td>{{plugin['os-plugin-name']}}</td>
+                        <td>{{plugin['version']}}</td>
+                        <td>{{$filters.date(+plugin['built-on'])}}</td>
+                        <td>{{plugin['commit']}}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </os-tab>
+              </os-tabs>
+            </template>
+            <template #footer>
+              <div class="help-footer">
+                <span>
+                  <a href="https://www.openspecimen.org" target="_blank" rel="noopener">OpenSpecimen</a>
+                </span>
+                <span>
+                  <span>- powered by</span>
+                  <a href="https://www.krishagni.com" target="_blank" rel="noopener">Krishagni</a>
+                </span>
+              </div>
+            </template>
+          </os-dialog>
+        </div>
+
         <div class="notifs">
           <button @click="toggleNotifOverlay">
             <os-icon name="bell" v-os-badge.danger="unreadNotifCount" v-if="unreadNotifCount > 0" />
@@ -32,9 +127,9 @@
         </div>
 
         <div class="user-profile">
-          <a @click="toggleProfileMenu">
+          <button @click="toggleProfileMenu">
             <os-username-avatar :name="username" />
-          </a>
+          </button>
 
           <os-overlay ref="userProfileMenu" @click="toggleProfileMenu">
             <ul class="user-profile-options">
@@ -145,6 +240,14 @@ export default {
   },
 
   methods: {
+    toggleHelpMenu: function(event) {
+      this.$refs.helpMenu.toggle(event);
+    },
+
+    showOpenSpecimenInfo: function() {
+      this.$refs.aboutDialog.open();
+    },
+
     toggleProfileMenu: function(event) {
       this.$refs.userProfileMenu.toggle(event);
     },
@@ -239,17 +342,27 @@ export default {
   cursor: pointer;
 }
 
-.user-profile,
-.user-profile a {
+.help-footer {
+  padding: 0.75rem 1rem;
+  text-align: center;
+}
+
+.user-profile {
   display: inline-block;
 }
 
+.user-profile button {
+  padding-top: 0rem;
+}
+
+.help-options,
 .user-profile-options {
   margin: -1.25rem;
   list-style: none;
   padding: 0.5rem 0rem;
 }
 
+.help-options li a,
 .user-profile-options li a {
   display: inline-block;
   padding: 0.75rem 1rem;
@@ -259,10 +372,12 @@ export default {
   width: 100%;
 }
 
+.help-options li:not(.divider):hover,
 .user-profile-options li:not(.divider):hover {
   background: #e9ecef;
 }
 
+.help-options li.divider,
 .user-profile-options li.divider {
   padding: 0.25rem 0rem;
   margin: -1rem 0rem;
