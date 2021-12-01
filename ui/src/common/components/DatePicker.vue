c8b1f501d059f8ad0f4c774d8bd3f3e5cb406383
Vinayak Pawar <vinayakapawar@gmail.com>
2021-12-01 07:01:49 +0530

    Date picker widget enhancements for use in the table form using material alike design (look & feel).

--- ui/src/common/components/DatePicker.vue
+++ ui/src/common/components/DatePicker.vue
@@ -1,8 +1,18 @@
 
 <template>
-  <Calendar class="os-date-picker" v-model="inputValue" :show-time="showTime" :date-format="format" 
-    :show-icon="true" :month-navigator="true" :year-navigator="true" year-range="1900:2100"
-    :show-button-bar="true" :hide-on-date-time-select="true" />
+  <div class="os-date-picker">
+    <div class="p-float-label" :class="!$attrs.placeholder && 'no-label'" v-if="$attrs['md-type']">
+      <Calendar v-model="inputValue" :show-time="showTime" :date-format="format"
+        :show-icon="true" :month-navigator="true" :year-navigator="true" year-range="1900:2100"
+        :show-button-bar="true" :hide-on-date-time-select="true" />
+      <label>{{$attrs.placeholder}}</label>
+    </div>
+    <div v-else>
+      <Calendar v-model="inputValue" :show-time="showTime" :date-format="format"
+        :show-icon="true" :month-navigator="true" :year-navigator="true" year-range="1900:2100"
+        :show-button-bar="true" :hide-on-date-time-select="true" :placeholder="$attrs.placeholder" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -65,8 +75,39 @@ export default {
 
 <style scoped>
 
-.os-date-picker {
+.os-date-picker :deep(.p-calendar) {
   width: 100%;
 }
 
+.os-date-picker .p-float-label:not(.no-label) {
+  margin-top: 10px;
+}
+
+.os-date-picker .p-float-label :deep(.p-inputtext) {
+  border: 0px;
+  border-bottom: 2px solid #ced4da;
+  border-radius: 0px;
+  padding: 2px 0px;
+  box-shadow: none;
+}
+
+.os-date-picker .p-float-label :deep(.p-button) {
+  padding: 0;
+  border: 0;
+  border-bottom: 2px solid #ced4da;
+  border-radius: 0;
+}
+
+.os-date-picker .p-float-label :deep(.p-inputtext:enabled:focus) {
+  border-bottom-color: #007bff;
+}
+
+.os-date-picker .p-float-label :deep(label) {
+  left: 0rem;
+}
+
+.os-date-picker .p-float-label :deep(.p-inputtext:not(:enabled:focus) ~ label) {
+  color: #999;
+  opacity: 1;
+}
 </style>
