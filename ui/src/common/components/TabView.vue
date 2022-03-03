34ff01cb34b454c4d733e7a3b3ee79eb0d745e09
Vinayak Pawar <vinayakapawar@gmail.com>
2022-03-03 13:59:03 +0530

    Updated tabs view widget to programatically select the tab. The tab changed event now includes the index of the selected tab along with the index of the previously active tab.

--- ui/src/common/components/TabView.vue
+++ ui/src/common/components/TabView.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <TabView :lazy="lazy" @tab-change="tabChanged($event)">
+  <TabView :lazy="lazy" v-model:activeIndex="activeTab" @tab-change="tabChanged($event)">
     <TabPanel v-for="(tab, idx) of tabs" :header="tab.props && tab.props.header" :key="idx">
       <template #header v-if="tab.children && tab.children.header">
         <component :is="tab.children.header"></component>
@@ -25,6 +25,14 @@ export default {
     TabPanel
   },
 
+  data() {
+    return {
+      activeTab: 0,
+
+      previousTab: 0
+    }
+  },
+
   computed: {
     tabs: function() {
       const tabs = [];
@@ -39,7 +47,12 @@ export default {
   methods: {
     tabChanged: function({index}) {
       const tab = this.tabs[index];
-      this.$emit('tab-changed', { tab });
+      this.$emit('tab-changed', { activeTab: index, previousTab: this.previousTab, tab });
+      this.previousTab = index;
+    },
+
+    activate: function(index) {
+      setTimeout(() => this.previousTab = this.activeTab = index, 5);
     }
   }
 }
