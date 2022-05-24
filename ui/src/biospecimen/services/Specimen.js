190d3375b82450485842f9949e180129b9cdade9
Vinayak Pawar <vinayakapawar@gmail.com>
2022-05-24 06:24:55 +0530

    OPSMN-5901: Fixed the new specimen actions widget to use the user profile setting as well as system setting to decide whether to download the labels print command file or not.

--- ui/src/biospecimen/services/Specimen.js
+++ ui/src/biospecimen/services/Specimen.js
@@ -1,6 +1,8 @@
+
 import alertsSvc   from '@/common/services/Alerts.js';
 import http        from '@/common/services/HttpClient.js';
 import settingsSvc from '@/common/services/Setting.js';
+import ui          from '@/global.js';
 import util        from '@/common/services/Util.js';
 
 class Specimen {
@@ -17,28 +19,29 @@ class Specimen {
     });
   }
 
-  printLabels(input, outputFilename) {
-    const printQ   = http.post('specimen-label-printer', input);
-    const settingQ = settingsSvc.getSetting('administrative', 'download_labels_print_file');
-    return Promise.all([printQ, settingQ]).then(
-      (resps) => {
-        const job = resps[0];
-        if (util.isTrue(resps[1][0].value)) {
-          let url = http.getUrl('specimen-label-printer/output-file', {query: {jobId: job.id}});
-          if (outputFilename) {
-            outputFilename = outputFilename.replace(/\/|\\/g, '_');
-            url += '&filename=' + outputFilename;
-          }
+  async printLabels(input, outputFilename) {
+    const job = await http.post('specimen-label-printer', input);
 
-          http.downloadFile(url);
-          alertsSvc.info('Downloading labels print CSV file...');
-        } else {
-          alertsSvc.success('Specimen labels print job ' + job.id + ' created successfully');
-        }
+    let downloadEnabled = ui.currentUser.downloadLabelsPrintFile;
+    if (!downloadEnabled) {
+      const setting = await settingsSvc.getSetting('administrative', 'download_labels_print_file');
+      downloadEnabled = util.isTrue(setting[0].value);
+    }
 
-        return job;
+    if (downloadEnabled) {
+      let url = http.getUrl('specimen-label-printer/output-file', {query: {jobId: job.id}});
+      if (outputFilename) {
+        outputFilename = outputFilename.replace(/\/|\\/g, '_');
+        url += '&filename=' + outputFilename;
       }
-    );
+
+      http.downloadFile(url);
+      alertsSvc.info('Downloading labels print CSV file...');
+    } else {
+      alertsSvc.success('Specimen labels print job ' + job.id + ' created.');
+    }
+
+    return job;
   }
 
   bulkUpdate(specimens) {
