49fc5dc4f34c3edc193b2f0572083664fabc4f88
Vinayak Pawar <vinayakapawar@gmail.com>
2022-09-26 16:10:54 +0530

    Participant visits dropdown widget.
    Useful widget in configurable specimen collection workflows.

--- ui/src/biospecimen/components/index.js
+++ ui/src/biospecimen/components/index.js
@@ -1,11 +1,14 @@
-import PpidList from './PpidList.vue';
+import PpidList  from './PpidList.vue';
+import VisitList from './VisitList.vue';
 
 import fieldFactory from '@/common/services/FieldFactory.js';
 
 export default {
   install(app) {
     app.component('os-ppid-list', PpidList);
+    app.component('os-visit-list', VisitList);
 
     fieldFactory.registerComponent('ppid-list', 'os-ppid-list');
+    fieldFactory.registerComponent('visit-list', 'os-visit-list');
   }
 }
