49df23c3ac26c31cf5b5f71e62f0f8ed77549c55
Vinayak Pawar <vinayakapawar@gmail.com>
2023-01-19 17:53:54 +0530

    Made changes in the HttpClient to allow requests to be made to other hosts like the scanner machine etc.

--- ui/src/common/services/HttpClient.js
+++ ui/src/common/services/HttpClient.js
@@ -32,8 +32,8 @@ class HttpClient {
     }
   }
 
-  get(url, params) {
-    return this.promise('get', () => axios.get(this.getUrl(url), this.config(params)));
+  get(url, params, options) {
+    return this.promise('get', () => axios.get(this.getUrl(url), this.config(params, options)));
   }
 
   async post(url, data, params) {
@@ -51,7 +51,10 @@ class HttpClient {
   }
 
   getUrl(url, {query = ''} = {}) {
-    url = this.getServerAppUrl() + 'rest/ng/' + url;
+    if (url.indexOf('http://') != 0 && url.indexOf('https://') != 0) {
+      url = this.getServerAppUrl() + 'rest/ng/' + url;
+    }
+
     if (query && typeof query == 'object') {
       let qp = '';
       Object.keys(query).forEach(
@@ -113,7 +116,7 @@ class HttpClient {
     link.dispatchEvent(clickEvent);
   }
 
-  config(params) {
+  config(params, options = {}) {
     if (params) {
       params = Object.keys(params).reduce(
         (urlSearchParams, name) => {
@@ -134,7 +137,7 @@ class HttpClient {
       );
     }
 
-    return {headers: this.headers, params: params};
+    return {headers: this.headers, params: params, ...options};
   }
 
   notifyStart(method) {
