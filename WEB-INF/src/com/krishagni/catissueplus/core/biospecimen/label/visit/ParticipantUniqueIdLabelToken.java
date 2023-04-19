737b99aa39b499708f53007c3dc4868f88af879b
Vinayak Pawar <vinayakapawar@gmail.com>
2023-04-19 15:12:11 +0530

    OPSMN-6198: Monotonically incrementing counter for use in visit names. The counter is incremented for every visit of the participant across multiple CPs.
    Token: %PUID(n)%

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/label/visit/ParticipantUniqueIdLabelToken.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/label/visit/ParticipantUniqueIdLabelToken.java
@@ -1,2 +1,25 @@
-package com.krishagni.catissueplus.core.biospecimen.label.visit;public class ParticipantUniqueIdLabelToken {
+package com.krishagni.catissueplus.core.biospecimen.label.visit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.krishagni.catissueplus.core.biospecimen.domain.Visit;
+import com.krishagni.catissueplus.core.biospecimen.repository.DaoFactory;
+import com.krishagni.catissueplus.core.common.domain.AbstractUniqueIdToken;
+
+public class ParticipantUniqueIdLabelToken extends AbstractUniqueIdToken<Visit> {
+
+	@Autowired
+	private DaoFactory daoFactory;
+
+	public ParticipantUniqueIdLabelToken() {
+		super();
+		this.type = "VISIT_NAME";
+		this.name = "PUID";
+	}
+
+	@Override
+	public Number getUniqueId(Visit visit, String... args) {
+		Long pid = visit.getRegistration().getParticipant().getId();
+		return daoFactory.getUniqueIdGenerator().getUniqueId(getTypeKey(), pid.toString());
+	}
 }
