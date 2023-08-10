1085c4997fd93c7e26a114d186142f8982e2227a
Vinayak Pawar <vinayakapawar@gmail.com>
2023-08-10 06:46:15 +0530

    SMS service interface.

--- WEB-INF/src/com/krishagni/catissueplus/core/common/service/SmsService.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/common/service/SmsService.java
@@ -3,7 +3,7 @@ package com.krishagni.catissueplus.core.common.service;
 import java.util.Map;
 
 public interface SmsService {
-	boolean sendMessage(String providerName, String to, String message);
+	String sendMessage(String providerName, String to, String message);
 
-	boolean sendMessage(String providerName, String to, String message, Map<String, Object> props);
+	String sendMessage(String providerName, String to, String message, Map<String, Object> props);
 }
