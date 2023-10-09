6c8f4ab231e51fb1c33278f64cc7b01ba7122745
Vinayak Pawar <vinayakapawar@gmail.com>
2023-10-09 11:23:49 +0530

    Specimen collection tree task: Display the specimen status ball in the description column.

--- ui/src/common/components/SpecimenDescription.vue
+++ ui/src/common/components/SpecimenDescription.vue
@@ -1,5 +1,6 @@
 <template>
-  <span>
+  <span class="description">
+    <span class="status-ball" :class="statusColor" v-if="showStatus && statusColor"></span>
     <span>{{specimen.type}}</span>
   </span>
 </template>
@@ -7,8 +8,52 @@
 <script>
 
 export default {
-  props: ['specimen']
+  props: ['specimen', 'show-status', 'status'],
+
+  computed: {
+    statusColor: function() {
+      if (this.specimen.activityStatus == 'Closed') {
+        return 'closed';
+      } else if (!this.status || this.status == 'Pending') {
+        return 'pending';
+      } else if (this.status == 'Missed Collection' || this.status == 'Not Collected') {
+        return 'not-collected';
+      } else if (this.status == 'Collected') {
+        return 'collected';
+      }
+
+      return null;
+    }
+  }
 }
 
 </script>
+
+<style scoped>
+.status-ball {
+  display: inline-block;
+  height: 0.75rem;
+  width: 0.75rem;
+  border-radius: 50%;
+  background: #a0a0a0;
+  margin-right: 0.25rem;
+  margin-bottom: -0.0625rem;
+}
+
+.status-ball.collected {
+  background: #5cb85c;
+}
+
+.status-ball.not-collected {
+  background: #888;
+}
+
+.status-ball.pending {
+  background: #f0ad4e;
+}
+
+.status-ball.closed {
+  background: #d9534f!important;
+}
+</style>
 
