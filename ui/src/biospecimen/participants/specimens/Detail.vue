e0c40d5d5daba8acff1be080d2b97529d17e0527
Vinayak Pawar <vinayakapawar@gmail.com>
2024-01-25 08:28:04 +0530

    New UI: Specimen status & color coding on the overview/detail page.

--- ui/src/biospecimen/participants/specimens/Detail.vue
+++ ui/src/biospecimen/participants/specimens/Detail.vue
@@ -11,6 +11,20 @@
         <h3>
           <span>{{specimen.label || specimen.type}}</span>
         </h3>
+
+        <div class="accessories">
+          <os-tag :value="status" :rounded="true" :type="statusType" />
+
+          <os-copy-link size="small" :route="{
+            name: 'ParticipantsListItemSpecimenDetail.Overview',
+            params: {cpId: ctx.cp.id, cprId: cpr.id, visitId: visit.id, specimenId: specimen.id},
+            query: {eventId: visit.eventId, srId: specimen.reqId}}" />
+
+          <os-new-tab size="small" :route="{
+            name: 'ParticipantsListItemSpecimenDetail.Overview',
+            params: {cpId: ctx.cp.id, cprId: cpr.id, visitId: visit.id, specimenId: specimen.id},
+            query: {eventId: visit.eventId, srId: specimen.reqId}}" />
+        </div>
       </span>
     </os-page-head>
     <os-page-body>
@@ -50,8 +64,12 @@
 export default {
   props: ['cpr', 'visit', 'specimen', 'noNavButton'],
 
+  inject: ['cpViewCtx'],
+
   data() {
     const ctx = {
+      cp: {},
+
       bcrumb: [
         // {url: routerSvc.getUrl('ParticipantsList', {cprId: -1}), label: cp.shortTitle}, // TODO: CP conf list view
         // {url: routerSvc.getUrl('ParticipantsList', {cprId: -1}), label: cpr.ppid}
@@ -62,11 +80,38 @@ export default {
   },
 
   created() {
+    this.cpViewCtx.getCp().then(cp => this.ctx.cp = cp);
+
     const route = this.$route.matched[this.$route.matched.length - 1];
     this.detailRouteName = route.name.split('.')[0];
     this.query = {};
     if (this.$route.query) {
       Object.assign(this.query, {filters: this.$route.query.filters});
+    }
+  },
+
+  computed: {
+    status: function() {
+      return this.specimen.availabilityStatus || 'Pending';
+    },
+
+    statusType: function() {
+      switch(this.status) {
+        case 'Available':
+          return 'success';
+        case 'Distributed':
+          return 'distributed';
+        case 'Reserved':
+          return 'reserved';
+        case 'Closed':
+          return 'danger';
+        case 'Missed Collection':
+        case 'Not Collected':
+          return 'missed';
+        case 'Pending':
+        default:
+          return 'warning';
+      }
     }
   },
 
--- ui/src/common/components/Tag.vue
+++ ui/src/common/components/Tag.vue
@@ -17,7 +17,11 @@ export default {
 
   computed: {
     containerClass: function() {
-      return ['os-tag', {'os-missed-tag': this.type == 'missed' || this.type == 'Missed'}]
+      return ['os-tag', {
+        'os-missed-tag': this.type == 'missed' || this.type == 'Missed',
+        'os-distributed-tag': this.type == 'distributed' || this.type == 'Distributed',
+        'os-reserved-tag': this.type == 'reserved' || this.type == 'Reserved'
+      }]
     }
   }
 }
@@ -31,6 +35,16 @@ export default {
 
 .os-tag.os-missed-tag :deep(.p-tag) {
   background-color: #888;
+  color: #fff;
+}
+
+.os-tag.os-distributed-tag :deep(.p-tag) {
+  background-color: #5bc0de;
+  color: #fff;
+}
+
+.os-tag.os-reserved-tag :deep(.p-tag) {
+  background-color: rgba(128, 0, 128, 0.7);
   color: #fff;
 }
 </style>
