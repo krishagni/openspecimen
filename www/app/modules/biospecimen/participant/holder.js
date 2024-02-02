79130604f8a361fc241988e70d541c9a0356adff
Vinayak Pawar <vinayakapawar@gmail.com>
2024-02-02 09:55:57 +0530

    New UI: Updated participants holder to read the selected participants from the local storage...

--- www/app/modules/biospecimen/participant/holder.js
+++ www/app/modules/biospecimen/participant/holder.js
@@ -1,15 +1,30 @@
 
 angular.module('os.biospecimen.participant')
-  .factory('ParticipantsHolder', function() {
+  .factory('ParticipantsHolder', function(CollectionProtocolRegistration) {
      this.participants = undefined;
 
      return {
        getParticipants: function() {
-         return this.participants;
+         if (this.participants) {
+           return this.participants;
+         }
+
+         var json = localStorage.getItem('os.participants');
+         if (json) {
+           return JSON.parse(json).map(function(cpr) { return new CollectionProtocolRegistration(cpr); });
+         }
+
+         return [];
        },
 
        setParticipants: function(participants) {
          this.participants = participants;
+         if (!this.participants) {
+           localStorage.removeItem('os.participants');
+         } else {
+           var json = JSON.stringify(participants.map(function(participant) { return {id: participant.id}; }));
+           localStorage.setItem('os.participants', json);
+         }
        }
-     }     
+     }
   });
