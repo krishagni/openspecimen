f4e581b02f5d849ebb2fc109278e7ea848069ed0
Vinayak Pawar <vinayakapawar@gmail.com>
2024-02-14 22:08:45 +0530

    OPSMN-6436: Allow transfer of selected participants from the angular view to the new Vue views...

--- www/app/modules/biospecimen/participant/holder.js
+++ www/app/modules/biospecimen/participant/holder.js
@@ -10,6 +10,12 @@ angular.module('os.biospecimen.participant')
 
        setParticipants: function(participants) {
          this.participants = participants;
+         if (!this.participants) {
+           localStorage.removeItem('os.participants');
+         } else {
+           var json = JSON.stringify(participants.map(function(p) { return {id: p.id}; }));
+           localStorage.setItem('os.participants', json);
+         }
        }
      }     
   });
