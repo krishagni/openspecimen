32fd51e3ea7636b94b4efff7f0fbc8afb4f9bad0
Vinayak Pawar <vinayakapawar@gmail.com>
2024-09-03 16:16:39 +0530

    OPSMN-6613: Implemented pagination to view all containers of the shipment...

--- ui/src/administrative/shipments/Containers.vue
+++ ui/src/administrative/shipments/Containers.vue
@@ -8,38 +8,70 @@
           :query="ctx.query"
           :allowSelection="false"
           :loading="ctx.loading"
-          ref="listView"
-        />
+          ref="listView">
+          <template #footer>
+            <os-pager :start-at="ctx.startAt" :have-more="ctx.haveMoreContainers"
+              @previous="previousPage" @next="nextPage" />
+          </template>
+        </os-list-view>
       </os-grid-column>
     </os-grid>
   </div>
 </template>
 
 <script>
-
-import { reactive } from 'vue';
-
 import listSchema  from '@/administrative/schemas/shipments/containers.js';
 import shipmentSvc from '@/administrative/services/Shipment.js';
+
+const MAX_CONTAINERS = 25;
 
 export default {
   props: ['shipment'],
 
-  setup(props) {
-    const ctx = reactive({
-      containers: [],
-      loading: true
-    });
+  data() {
+    return {
+      ctx: {
+        containers: [],
+        loading: true,
+        startAt: 0,
+        haveMoreContainers: false
+      },
 
-    ctx.loading = true;
-    shipmentSvc.getContainers(props.shipment.id).then(
-      (containers) => {
-        ctx.loading = false;
-        ctx.containers = containers;
-      }
-    );
-
-    return { ctx, listSchema };
+      listSchema
+    }
   },
+
+  created() {
+    this.loadContainers(0);
+  },
+
+
+  methods: {
+    loadContainers: async function(startAt) {
+      const ctx = this.ctx;
+      ctx.loading = true;
+
+      const containers = await shipmentSvc.getContainers(
+        this.shipment.id, {startAt: startAt, maxResults: MAX_CONTAINERS + 1});
+      if (containers.length > MAX_CONTAINERS) {
+        ctx.haveMoreContainers = true;
+        containers.splice(containers.length - 1, 1);
+      } else {
+        ctx.haveMoreContainers = false;
+      }
+
+      ctx.containers = containers;
+      ctx.startAt = startAt;
+      ctx.loading = false;
+    },
+
+    previousPage: function() {
+      this.loadContainers(this.ctx.startAt - MAX_CONTAINERS);
+    },
+
+    nextPage: function() {
+      this.loadContainers(this.ctx.startAt + MAX_CONTAINERS);
+    }
+  }
 }
 </script>
