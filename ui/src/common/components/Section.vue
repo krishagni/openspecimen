67212806e222cced8c130ecc84324dbdf2d8efcc
Vinayak Pawar <vinayakapawar@gmail.com>
2024-10-12 08:53:07 +0530

    Fixed the section widget to allow users collapse/expand longer sections...

--- ui/src/common/components/Section.vue
+++ ui/src/common/components/Section.vue
@@ -1,14 +1,24 @@
 
 <template>
   <div class="os-section os-line-section">
-    <strong class="title"><slot name="title"> </slot></strong>
-    <pre class="content"><slot name="content"> </slot></pre>
+    <span class="title-wrapper">
+      <os-icon name="chevron-circle-right" v-show="collapsed" @click="collapsed=!collapsed"></os-icon>
+      <os-icon name="chevron-circle-down" v-show="!collapsed" @click="collapsed=!collapsed"></os-icon>
+      <strong class="title"><slot name="title"> </slot></strong>
+    </span>
+    <pre class="content" v-show="!collapsed"><slot name="content"> </slot></pre>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['title', 'content']
+  props: ['title', 'content'],
+
+  data() {
+    return {
+      collapsed: false
+    }
+  }
 }
 </script>
 
@@ -30,5 +40,21 @@ export default {
 .os-line-section > .title {
   background: #ffffff;
 }
+
+.os-section .title-wrapper {
+  display: flex;
+  flex-direction: row;
+}
+
+.os-section .title-wrapper :deep(.os-icon-wrapper) {
+  color: #707070;
+  padding-right: 0.25rem;
+  cursor: pointer;
+}
+
+.os-section .title-wrapper .title {
+  margin-top: -1px;
+}
+
 
 </style>
