eb22d928768d8127bc515c95775cf9507f7478da
Vinayak Pawar <vinayakapawar@gmail.com>
2024-12-03 14:30:17 +0530

    OPSMN-6722: Specimen requirement exporter and importer fixes to retain the hierarchical relationship between requirements (sans SR codes) when exporting and importing on 2 different instances.
    1. UID and Parent UID is added when exporting the requirements.
    These are transient fields that do not exist in the database or query results.
    
    2. The UIDs if present are used by the importer to identify & create the parent-child relationship.
    
    3. UID is used only if the SR codes cannot be used

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/SpecimenRequirementImporter.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/SpecimenRequirementImporter.java
@@ -1,6 +1,8 @@
 package com.krishagni.catissueplus.core.biospecimen.services.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
@@ -15,60 +17,99 @@ import com.krishagni.catissueplus.core.common.events.RequestEvent;
 import com.krishagni.catissueplus.core.common.events.ResponseEvent;
 import com.krishagni.catissueplus.core.importer.events.ImportObjectDetail;
 import com.krishagni.catissueplus.core.importer.services.ObjectImporter;
+import com.krishagni.catissueplus.core.importer.services.ObjectImporterLifecycle;
 
-public class SpecimenRequirementImporter implements ObjectImporter<SpecimenRequirementDetail, SpecimenRequirementDetail> {
+public class SpecimenRequirementImporter implements ObjectImporter<SpecimenRequirementDetail, SpecimenRequirementDetail>, ObjectImporterLifecycle {
 	private CollectionProtocolService cpSvc;
+
+	private Map<String, Map<String, Long>> contextMap = new HashMap<>();
 
 	public void setCpSvc(CollectionProtocolService cpSvc) {
 		this.cpSvc = cpSvc;
 	}
 
 	@Override
+	public void start(String id) {
+		contextMap.put(id, new HashMap<>());
+	}
+
+	@Override
+	public void stop(String id, Map<String, Object> runCtxt) {
+		contextMap.remove(id);
+	}
+
+	@Override
 	public ResponseEvent<SpecimenRequirementDetail> importObject(RequestEvent<ImportObjectDetail<SpecimenRequirementDetail>> req) {
 		try {
 			ImportObjectDetail<SpecimenRequirementDetail> importDetail = req.getPayload();
-			SpecimenRequirementDetail detail = importDetail.getObject();
 
+			SpecimenRequirementDetail detail = importDetail.getObject();
 			if (!importDetail.isCreate()) {
 				return cpSvc.updateSpecimenRequirement(new RequestEvent<>(detail));
 			}
 
+			Map<String, Long> srIds = contextMap.get(importDetail.getId());
+			ResponseEvent<SpecimenRequirementDetail> resp = null;
 			String lineage = detail.getLineage();
 			if (StringUtils.isBlank(lineage) || StringUtils.equalsIgnoreCase(lineage, Specimen.NEW)) {
-				return importPrimaryRequirements(detail);
-			} else if (StringUtils.equalsIgnoreCase(lineage, Specimen.ALIQUOT)) {
-				return importAliquots(detail);
-			} else if (StringUtils.equalsIgnoreCase(lineage, Specimen.DERIVED)) {
-				return importDerivatives(detail);
+				resp = importPrimaryRequirements(detail);
 			} else {
-				return ResponseEvent.userError(SrErrorCode.INVALID_LINEAGE);
+				String cpShortTitle = detail.getCpShortTitle();
+				String eventLabel   = detail.getEventLabel();
+				String parentSrCode = detail.getParentSrCode();
+				Long parentSrId     = null;
+
+				if (StringUtils.isBlank(cpShortTitle) || StringUtils.isBlank(eventLabel) || StringUtils.isBlank(parentSrCode)) {
+					if (StringUtils.isNotBlank(detail.getParentUid())) {
+						parentSrId = srIds.get(detail.getParentUid());
+					}
+				}
+
+				if (StringUtils.equalsIgnoreCase(lineage, Specimen.ALIQUOT)) {
+					resp = importAliquots(parentSrId, detail);
+				} else if (StringUtils.equalsIgnoreCase(lineage, Specimen.DERIVED)) {
+					resp = importDerivatives(parentSrId, detail);
+				} else {
+					resp = ResponseEvent.userError(SrErrorCode.INVALID_LINEAGE);
+				}
 			}
-			
+
+			if (resp.isSuccessful() && StringUtils.isNotBlank(detail.getUid())) {
+				srIds.put(detail.getUid(), resp.getPayload().getId());
+			}
+
+			return resp;
 		} catch (Exception e) {
 			return ResponseEvent.serverError(e);
 		}
 	}
 	
 	private ResponseEvent<SpecimenRequirementDetail> importPrimaryRequirements(SpecimenRequirementDetail detail) {
-		return cpSvc.addSpecimenRequirement(new RequestEvent<SpecimenRequirementDetail>(detail));
+		return cpSvc.addSpecimenRequirement(new RequestEvent<>(detail));
 	}
 	
-	private ResponseEvent<SpecimenRequirementDetail> importAliquots(SpecimenRequirementDetail detail) {
+	private ResponseEvent<SpecimenRequirementDetail> importAliquots(Long parentSrId, SpecimenRequirementDetail detail) {
 		AliquotSpecimensRequirement aliquotDetail = new AliquotSpecimensRequirement();
 		BeanUtils.copyProperties(detail, aliquotDetail);
 		aliquotDetail.setNoOfAliquots(1);
 		aliquotDetail.setQtyPerAliquot(detail.getInitialQty());
+		if (parentSrId != null) {
+			aliquotDetail.setParentSrId(parentSrId);
+		}
 			
-		ResponseEvent<List<SpecimenRequirementDetail>> resp = cpSvc.createAliquots(new RequestEvent<AliquotSpecimensRequirement>(aliquotDetail));
+		ResponseEvent<List<SpecimenRequirementDetail>> resp = cpSvc.createAliquots(new RequestEvent<>(aliquotDetail));
 		resp.throwErrorIfUnsuccessful();
-		return new ResponseEvent<SpecimenRequirementDetail>(resp.getPayload().iterator().next());
+		return new ResponseEvent<>(resp.getPayload().iterator().next());
 	}
 	
-	private ResponseEvent<SpecimenRequirementDetail> importDerivatives(SpecimenRequirementDetail detail) {
+	private ResponseEvent<SpecimenRequirementDetail> importDerivatives(Long parentSrId, SpecimenRequirementDetail detail) {
 		DerivedSpecimenRequirement derivativeDetail = new DerivedSpecimenRequirement();
 		BeanUtils.copyProperties(detail, derivativeDetail);
 		derivativeDetail.setQuantity(detail.getInitialQty());
+		if (parentSrId != null) {
+			derivativeDetail.setParentSrId(parentSrId);
+		}
 			
-		return cpSvc.createDerived(new RequestEvent<DerivedSpecimenRequirement>(derivativeDetail));
+		return cpSvc.createDerived(new RequestEvent<>(derivativeDetail));
 	}
 }
