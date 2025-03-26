bc5548e4758de0ff0641ba4a374cbb80d7124327
Vinayak Pawar <vinayakapawar@gmail.com>
2025-03-26 20:29:40 +0530

    Naked app UI shell (2/2)

---
+++ ui/src/AppShell.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="os-root">
+    <os-app-body />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoLoginApp',
+}
+</script>
+
+<style scoped>
+.os-root {
+  height: 100%;
+}
+
+.os-root :deep(.os-app-body) {
+  height: 100%;
+}
+</style>
