7b144a942b2c0f9b23492d9cf644ff246a9fff50
Vinayak Pawar <vinayakapawar@gmail.com>
2025-04-18 10:21:50 +0530

    Button group CSS fixes

--- ui/src/common/components/ButtonGroup.vue
+++ ui/src/common/components/ButtonGroup.vue
@@ -12,12 +12,14 @@ export default {
 
 <style scoped>
 
-.os-btn-group :deep(button:not(:last-child)) {
+.os-btn-group :deep(> *:not(:last-child)),
+.os-btn-group :deep(> *:not(:last-child) button) {
   border-top-right-radius: 0px;
   border-bottom-right-radius: 0px;
 }
 
-.os-btn-group :deep(button:not(:first-child)) {
+.os-btn-group :deep(> *:not(:first-child)),
+.os-btn-group :deep(> *:not(:first-child) button) {
   border-top-left-radius: 0px;
   border-bottom-left-radius: 0px;
   border-left: 0px;
