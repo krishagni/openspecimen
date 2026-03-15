97cd685d5da17130e9e2685b104f827fb9717a31
Vinayak Pawar <vinayakapawar@gmail.com>
2026-03-15 07:03:20 +0530

    OPSMN-7189: Refactored "Refresh Participant Specimen Stats" scheduled job.
    Earlier approach: A single query computed collected, missed, not collected,
    pending specimen counts for every participant in the database. The output
    was subsequently used to create a new stats table.
    
    While this approach worked well for databases with smaller number of participants
    and specimens, it started failing on larger databases due to large transaction
    size and connection timeouts.
    
    Redesign:
    In view of the above, the the job is redesigned to populate the stats in chunks.
    By default, participants are divided in chunks of 10K. The job iterates through
    each chunk to compute and insert the stats sequentially.
    
    While this might not significantly reduce the execution time, it keeps the
    transaction sizes in check and avoids connection timeouts.
    
    Users (super admin) can fine tune the right chunk size for their database by
    specifying a numeric value in the "Fixed Parameters" text box of the add/edit
    job UI form.

--- WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/RefreshCprSpecimenStats.java
+++ WEB-INF/src/com/krishagni/catissueplus/core/biospecimen/services/impl/RefreshCprSpecimenStats.java
@@ -1,5 +1,10 @@
 package com.krishagni.catissueplus.core.biospecimen.services.impl;
 
+import java.sql.CallableStatement;
+import java.sql.Types;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -11,15 +16,54 @@ import com.krishagni.catissueplus.core.common.PlusTransactional;
 @Configurable
 public class RefreshCprSpecimenStats implements ScheduledTask {
 
+	private static final int DEFAULT_CHUNK_SIZE = 10000;
+
 	@Autowired
 	private SessionFactory sessionFactory;
 
 	@Override
-	@PlusTransactional
 	public void doJob(ScheduledJobRun jobRun)
 	throws Exception {
-		sessionFactory.getCurrentSession()
-			.createSQLQuery("call refresh_cpr_spmn_stats()")
-			.executeUpdate();
+		int chunkSize = getChunkSize(jobRun);
+		long lastId = 0L;
+		long prevLastId = -1L;
+		while (prevLastId != lastId) {
+			prevLastId = lastId;
+			lastId = invokeRefreshProc(prevLastId, chunkSize);
+		}
+	}
+
+	@PlusTransactional
+	private long invokeRefreshProc(long lastId, int chunkSize) {
+		AtomicLong updatedLastId = new AtomicLong(lastId);
+		sessionFactory.getCurrentSession().doWork(
+			connection -> {
+				try (CallableStatement stmt = connection.prepareCall("{call refresh_cpr_spmn_stats(?, ?, ?)}")) {
+					stmt.setLong(1, lastId);
+					stmt.setInt(2, chunkSize);
+					stmt.registerOutParameter(3, Types.BIGINT);
+					stmt.execute();
+
+					long value = stmt.getLong(3);
+					updatedLastId.set(stmt.wasNull() ? lastId : value);
+				}
+			}
+		);
+
+		return updatedLastId.get();
+	}
+
+	private int getChunkSize(ScheduledJobRun jobRun) {
+		String args = jobRun.getScheduledJob().getFixedArgs();
+		if (StringUtils.isBlank(args)) {
+			return DEFAULT_CHUNK_SIZE;
+		}
+
+		try {
+			int value = Integer.parseInt(args.trim());
+			return value > 0 ? value : DEFAULT_CHUNK_SIZE;
+		} catch (Exception e) {
+			return DEFAULT_CHUNK_SIZE;
+		}
 	}
 }
