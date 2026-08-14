866d7ebe173f2d7902346fdbafa77f9268f8e9b3
Vinayak Pawar <vinayakapawar@gmail.com>
2026-08-14 09:38:20 +0530

    OPSMN-7385: Show the legacy Dropdown, MultiSelect Dropdown, Radio, and Checkboxes.
    Hide the newer PV-based Radio and Checkboxes.
    
    Rename the PV based "Dropdown" to "Permissible Value"
    
    Reason: Having many PV based fields means the query module would have to join
    with the PV table for each field appearing in the query either a filter, or
    select field, or grouping category. There's a hard limit of 61 joins imposed by
    MySQL. In view of this, we are taking one step back to ensure users are not
    impacted by this and solve the query problem before moving to PV based fields.

--- ui/src/administrative/forms/designer/services/Utility.js
+++ ui/src/administrative/forms/designer/services/Utility.js
@@ -51,7 +51,7 @@ class Utility {
     getInterchangeableTypes(field) {
         if (field.type == 'radiobutton') {
           return [
-            // { name: 'combobox', caption: 'Dropdown' },
+            { name: 'combobox', caption: 'Dropdown' },
             { name: 'stringTextField', caption: 'Text Field' }
           ]
         } else if (field.type == 'combobox') {
@@ -59,21 +59,21 @@ class Utility {
             { name: 'radiobutton', caption: 'Radio Button' },
             { name: 'stringTextField', caption: 'Text Field' }
           ]
-        // } else if (field.type == 'checkbox') {
-        //   return [
-        //     { name: 'multiSelectListbox', caption: 'Multiselect Dropdown' }
-        //   ]
+        } else if (field.type == 'checkbox') {
+          return [
+            { name: 'multiSelectListbox', caption: 'Multiselect Dropdown' }
+          ]
         } else if (field.type == 'multiSelectListbox') {
           return [
             { name: 'checkbox', caption: 'Checkbox' }
           ]
         } else if (field.type == 'pvRadioButton') {
           return [
-            { name: 'pvField', caption: 'Dropdown', multiple: false }
+            { name: 'pvField', caption: 'Permissible Value', multiple: false }
           ]
         } else if (field.type == 'pvCheckbox') {
           return [
-            { name: 'pvField', caption: 'Dropdown', multiple: true }
+            { name: 'pvField', caption: 'Permissible Value', multiple: true }
           ]
         }
 
